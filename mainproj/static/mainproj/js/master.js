a935e2a29082aad96b2a49b52858c7c91c9dec7d
bfolks2 <benfolks@Bens-MacBook-Pro.local>
2018-06-24 22:13:36 -0500

    update js stuff

--- mainproj/static/mainproj/js/master.js
+++ mainproj/static/mainproj/js/master.js
@@ -44,49 +44,49 @@ function getQuote(randnum)
   switch (randnum)
   {
     case 1:
-      return "Character is built through adversity."
+      return "Character is built through adversity.";
     case 2:
       return "You don’t always need a plan.  Sometimes you just need to breathe, trust, let go, and see what happens.  " +
-      "(Neither this website nor its non-existent subsidiaries are liable for what happens)"
+      "(Neither this website nor its non-existent subsidiaries are liable for what happens)";
     case 3:
-      return "Go past the max!  Reach over the top!"
+      return "Go past the max!  Reach over the top!";
     case 4:
-      return "Get back to work!"
+      return "Get back to work!";
     case 5:
-      return "I think I can, I think I can."
+      return "I think I can, I think I can.";
     case 6:
       return "I guess if a person never quit when the going got tough, they wouldn't have anything to regret for the rest" +
-      "  of their life...  But good luck to you. I'm sure this decision won't haunt you forever."
+      "  of their life...  But good luck to you. I'm sure this decision won't haunt you forever.";
     case 7:
-      return "Milk is for babies. When you grow up, you have to drink beer."
+      return "Milk is for babies. When you grow up, you have to drink beer.";
     case 8:
-      return "Never give up.  Trust your instincts."
+      return "Never give up.  Trust your instincts.";
     case 9:
-      return "You haven't failed.  You've just found 10,000 ways that won't work."
+      return "You haven't failed.  You've just found 10,000 ways that won't work.";
     case 10:
-      return "Don't let anyone ever dull your sparkle."
+      return "Don't let anyone ever dull your sparkle.";
     case 11:
-      return "Bad luck and extreme misfortune will infest your pathetic soul for all eternity."
+      return "Bad luck and extreme misfortune will infest your pathetic soul for all eternity.";
     case 12:
-      return "What's the worst that could happen?"
+      return "What's the worst that could happen?";
     case 13:
-      return "If wishes were horses, we'd all be eating steak."
+      return "If wishes were horses, we'd all be eating steak.";
     case 14:
       return "When I started with computers, we didn't have any fancy-schmanzy internet. We had sticks!" +
-      "  Two sticks, and a rock for the whole group.  And we had to share the rock!  Buck up, you are one very lucky person."
+      "  Two sticks, and a rock for the whole group.  And we had to share the rock!  Buck up, you are one very lucky person.";
     case 15:
-      return "I just want to tell you good luck. We're all counting on you."
+      return "I just want to tell you good luck. We're all counting on you.";
     case 16:
-      return "Nobody exists on purpose, nobody belongs anywhere, everybody's gonna die. Please hire me :)"
+      return "Nobody exists on purpose, nobody belongs anywhere, everybody's gonna die.";
     case 17:
-      return "Just remember, win or lose… those are the two options:<br>WIN... or lose."
+      return "Just remember, win or lose… those are the two options:<br>WIN... or lose.";
     case 18:
-      return "This website is pretty wonderful, isn't it????"
+      return "This website is pretty wonderful, isn't it????";
     case 19:
-      return "If you put your mind to it, you can accomplish anything."
+      return "If you put your mind to it, you can accomplish anything.";
     case 20:
-      return "2:40<br>2,2,1"
+      return "2:40<br>2,2,1";
     case 21:
-      return "Inside you is the potential to make yourself better, and that is what it is to be human.  To make yourself more than you are."
+      return "Inside you is the potential to make yourself better, and that is what it is to be human.  To make yourself more than you are.";
   }
 }
