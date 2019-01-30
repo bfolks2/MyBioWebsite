972fe80998e10b2fbfcf7dbf88b4f091040c15c2
Ben Folks <ben.folks@redshelf.com>
2019-01-30 17:52:08 -0600

    added scrum and SW pages

--- mainproj/mainapp/urls.py
+++ mainproj/mainapp/urls.py
@@ -7,5 +7,7 @@ urlpatterns=[
     url(r'^$',views.SkillList.as_view(),name='list'),
     url(r'^details/(?P<slug>[-\w]+)/$',views.SkillDetail.as_view(),name='details'),
     url(r'^experience/$',views.Documents.as_view(),name='documents'),
-    url(r'^resume/$', views.pdfview, name='pdf')
+    url(r'^resume/$', views.resume_pdfview, name='resume-pdf'),
+    url(r'^scrum/$', views.scrum_pdfview, name='scrum-pdf'),
+    url(r'^solidworks/$', views.solidworks_pdfview, name='sw-pdf'),
 ]
--- mainproj/mainapp/views.py
+++ mainproj/mainapp/views.py
@@ -4,19 +4,31 @@ from django.views import generic
 
 # Create your views here.
 
+
 def index(request):
     return render(request, 'index.html')
 
-def pdfview(request):
+
+def resume_pdfview(request):
     return render(request, 'mainapp/resume.html')
+
+
+def solidworks_pdfview(request):
+    return render(request, 'mainapp/solidworks.html')
+
+
+def scrum_pdfview(request):
+    return render(request, 'mainapp/scrum.html')
 
 class Documents(generic.TemplateView):
     template_name='mainapp/documents.html'
+
 
 class SkillList(generic.ListView):
     model=models.Skill
     template_name='mainapp/skill_list.html'
     context_object_name='skills'
+
 
 class SkillDetail(generic.DetailView):
     model=models.Skill
